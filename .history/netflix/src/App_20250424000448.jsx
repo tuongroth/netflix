0e706ab1b41c974a5d950b7233ad593ffe1c9e8a
TuongRoth <rothtrancattuong0703@gmail.com>
2025-04-24 10:11:53 +0300

    comit

--- .history/netflix/src/App_20250424000448.jsx
+++ .history/netflix/src/App_20250424000448.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Home from './pages/Home/Home'
+import H
 const App = () => {
   return (
     <div>
-      <Home />
+      hi
     </div>
   )
 }
--- .history/netflix/src/App_20250424000846.jsx
+++ .history/netflix/src/App_20250424000846.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home/Home'
 const App = () => {
   return (
     <div>
-      <Home />
+      <home />
     </div>
   )
 }
